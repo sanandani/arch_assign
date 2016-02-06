0f7360a193b68e4eb702d1afdbee3d541dd042e3
mewanbanjop-mawroh <mewanbanjop.mawroh@gmail.com>
2016-02-06 16:57:10 -0500

    Added functionality to readIdand readMeasurement

--- A1/Sample/SinkFilter.java
+++ A1/Sample/SinkFilter.java
@@ -28,6 +28,8 @@ import java.text.SimpleDateFormat;		// This class is used to format and write ti
 
 public class SinkFilter extends FilterFramework
 {
+	final int IdLength = 4;				// This is the length of IDs in the byte stream 
+	final int MeasurementLength = 8;	// This is the length of all measurements in bytes
 	public void run()
     {
 		/************************************************************************************
@@ -61,26 +63,11 @@ public class SinkFilter extends FilterFramework
 			{
 				/***************************************************************************
 				// We know that the first data coming to this filter is going to be an ID and
-				// that it is IdLength long. So we first decommutate the ID bytes.
+				// so we read Id first and increment bytes read by IdLength.
 				****************************************************************************/
 
-				id = 0;
-
-				for (i=0; i<IdLength; i++ )
-				{
-					databyte = ReadFilterInputPort();	// This is where we read the byte from the stream...
-
-					id = id | (databyte & 0xFF);		// We append the byte on to ID...
-
-					if (i != IdLength-1)				// If this is not the last byte, then slide the
-					{									// previously appended byte to the left by one byte
-						id = id << 8;					// to make room for the next byte we append to the ID
-
-					} // if
-
-					bytesread++;						// Increment the byte count
-
-				} // for
+				id = readId();
+				bytesread += IdLength;
 
 				/****************************************************************************
 				// Here we read measurements. All measurement data is read as a stream of bytes
@@ -94,22 +81,8 @@ public class SinkFilter extends FilterFramework
 				// below.
 				*****************************************************************************/
 
-				measurement = 0;
-
-				for (i=0; i<MeasurementLength; i++ )
-				{
-					databyte = ReadFilterInputPort();
-					measurement = measurement | (databyte & 0xFF);	// We append the byte on to measurement...
-
-					if (i != MeasurementLength-1)					// If this is not the last byte, then slide the
-					{												// previously appended byte to the left by one byte
-						measurement = measurement << 8;				// to make room for the next byte we append to the
-																	// measurement
-					} // if
-
-					bytesread++;									// Increment the byte count
-
-				} // if
+				measurement = readMeasurement();
+				measurement += MeasurementLength;
 
 				/****************************************************************************
 				// Here we look for an ID of 0 which indicates this is a time measurement.
@@ -165,5 +138,57 @@ public class SinkFilter extends FilterFramework
 		} // while
 
    } // run
+
+   	/*******************************************************************************
+	*	The 'readId' function is used to read the id from filter input port. All 
+	*	ids are read as stream of bytes and stored as integer.
+	********************************************************************************/
+
+	public int readId() throws EndOfStreamException
+    {
+		byte databyte = 0;				// This is the data byte read from the stream
+
+		int id = 0;						// This is the measurement id
+
+		for (int i=0; i<IdLength; i++ )
+		{
+			databyte = ReadFilterInputPort();	// This is where we read the byte from the stream...
+
+			id = id | (databyte & 0xFF);		// We append the byte on to ID...
+
+			if (i != IdLength-1)				// If this is not the last byte, then slide the
+			{									// previously appended byte to the left by one byte
+				id = id << 8;					// to make room for the next byte we append to the ID
+
+			}
+		}
+		return id;
+	}// readId
+
+	/*******************************************************************************
+	* The 'readMeasurement' function is used to read the measurements from filter input 
+	* port. All measurements are read as stream of bytes and stored as long value.
+	********************************************************************************/
+	public long readMeasurement() throws EndOfStreamException
+    {
+		byte databyte = 0;				// This is the data byte read from the stream
+
+		long measurement = 0;						// This is the measurement measurement
+
+		for (int i=0; i<MeasurementLength; i++ )
+		{
+			databyte = ReadFilterInputPort();	// This is where we read the byte from the stream...
+
+			measurement = measurement | (databyte & 0xFF);		// We append the byte on to ID...
+
+			if (i != MeasurementLength-1)				// If this is not the last byte, then slide the
+			{									// previously appended byte to the left by one byte
+				measurement = measurement << 8;					// to make room for the next byte we append to the ID
+
+			}
+		}
+		
+		return measurement;
+	}// readMeasurement
 
 } // SingFilter
