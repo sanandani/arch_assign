669ddf719f0e439b340e3df1b6aaedc3ad7424a5
mewanbanjop-mawroh <mewanbanjop.mawroh@gmail.com>
2016-02-09 01:55:44 -0500

    Done with ExtrapolateSink

--- A1/SystemB/ExtrapolatedPressureSink.java
+++ A1/SystemB/ExtrapolatedPressureSink.java
@@ -7,24 +7,28 @@ import java.util.Arrays;
 import java.util.Calendar;
 
 public class ExtrapolatedPressureSink extends InstrumentationFilter {
-	final int [] IDS_TO_WRITE = {0,1,2,3};
+	final int[] MEASUREMENT_IDS_TO_WRITE = { 0, 4, 5, 3 };
+	Calendar TimeStamp = Calendar.getInstance();
+	SimpleDateFormat TimeStampFormat = new SimpleDateFormat("yyyy MM dd::hh:mm:ss:SSS");
+
 	public void run() {
 		int bytesread = 0, numberOfBytes = 0, indexOfWildPoint = 0;
 		ArrayList<InstrumentationData> currentRecord = null;
 		try {
+
 			BufferedWriter out = new BufferedWriter(new FileWriter("SystemB.txt"));
-			
 			System.out.print("\n" + "ExtrapolatedPressureSink" + "::Reading ");
-			out.write("Time:\t\t Temperature (C):\t\t Altitude (m):\t\t Pressure (psi):\n");
+			out.write("Time:\t\t\t\t\t\t Temperature (C):\t\t\t\t Altitude (m):\t\t\t\t Pressure (psi):\n");
 			currentRecord = readRecord(true);
 			numberOfBytes = 12 * currentRecord.size();
 			indexOfWildPoint = getRecordIndexOf(currentRecord, WILDPOINT_ID);
+			ArrayList<Integer> recordIndicesToWrite = getOrderedRecordIndicesToWrite(currentRecord);
 			
 			if (isCurrentWildPoint(currentRecord, indexOfWildPoint)) {
-				writeWildRecord(currentRecord, indexOfWildPoint, out);
+				writeWildRecord(currentRecord, out, recordIndicesToWrite );
 				bytesread += numberOfBytes;
 			} else {
-				writeNormalRecord(currentRecord, out);
+				writeNormalRecord(currentRecord, out, recordIndicesToWrite);
 				bytesread += numberOfBytes;
 			}
 			while (true) {
@@ -32,17 +36,17 @@ public class ExtrapolatedPressureSink extends InstrumentationFilter {
 					currentRecord = readRecord(false);
 
 					if (isCurrentWildPoint(currentRecord, indexOfWildPoint)) {
-						writeWildRecord(currentRecord, indexOfWildPoint, out);
+						writeWildRecord(currentRecord, out,recordIndicesToWrite );
 						bytesread += numberOfBytes;
 					} else {
-						writeNormalRecord(currentRecord, out);
+						writeNormalRecord(currentRecord, out, recordIndicesToWrite);
 						bytesread += numberOfBytes;
 					}
 
 				} catch (EndOfStreamException e) {
 					ClosePorts();
 					out.close();
-					System.out.print( "\nExtrapolatedPressureSink" + "Exiting; bytes read: " + bytesread );
+					System.out.print("\nExtrapolatedPressureSink" + "Exiting; bytes read: " + bytesread);
 					break;
 				} // catch
 
@@ -50,51 +54,65 @@ public class ExtrapolatedPressureSink extends InstrumentationFilter {
 		} // try
 		catch (EndOfStreamException e) {
 			ClosePorts();
-			System.out.print( "\nExtrapolatedPressureSink" + "Exiting; bytes read: " + bytesread );
+			System.out.print("\nExtrapolatedPressureSink" + "Exiting; bytes read: " + bytesread);
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}// run
 
-	private void writeNormalRecord(ArrayList<InstrumentationData> currentRecord, BufferedWriter out)
-			throws IOException {
-		for (int i = 0; i < currentRecord.size(); i++)
-			writeMeasurementToFile(currentRecord, out, i);
-
+	private ArrayList<Integer> getOrderedRecordIndicesToWrite(ArrayList<InstrumentationData> currentRecord) {
+		ArrayList<Integer> recordIndicesForWrite = new ArrayList<Integer>();
+		for (int i = 0; i < MEASUREMENT_IDS_TO_WRITE.length; i++) {
+			for (int j = 0; j < currentRecord.size(); j++) {
+				if (currentRecord.get(j).id == MEASUREMENT_IDS_TO_WRITE[i]) {
+					recordIndicesForWrite.add(j);
+					System.out.println("\n\n\nindex:"+j);
+					break;
+				}
+			}
+		}
+		return recordIndicesForWrite;
 	}
 
-	private void writeWildRecord(ArrayList<InstrumentationData> currentRecord, int indexOfWildPoint, BufferedWriter out)
+	private void writeNormalRecord(ArrayList<InstrumentationData> currentRecord, BufferedWriter out, ArrayList<Integer> recordIndicesToWrite)
 			throws IOException {
-//		System.out.println("\n\nWild******");
-		for (int i = 0; i < currentRecord.size(); i++) {
-			if (currentRecord.get(i).id == PRESSURE_ID) {
-//				System.out.println(Double.toString((Double.longBitsToDouble(currentRecord.get(i).measurement))) + "*"+"\t");
-				out.write(Double.toString((Double.longBitsToDouble(currentRecord.get(i).measurement))) + "*"+"\t");
+		for (int i = 0; i < recordIndicesToWrite.size(); i++)
+			writeMeasurementToFile(currentRecord, out, recordIndicesToWrite.get(i));
+	}
+
+	private void writeWildRecord(ArrayList<InstrumentationData> currentRecord, BufferedWriter out, ArrayList<Integer> recordIndicesToWrite)
+			throws IOException {
+		for (int i = 0; i < recordIndicesToWrite.size(); i++) {
+			int index = recordIndicesToWrite.get(i);
+			System.out.println("\n\n\nindex:"+index);
+			if (currentRecord.get(index).id == PRESSURE_ID) {
+				out.write(Double.toString((Double.longBitsToDouble(currentRecord.get(i).measurement))) + "*" + "\t");
 			} else {
-				writeMeasurementToFile(currentRecord, out, i);
+				writeMeasurementToFile(currentRecord, out, index);
 			}
 		}
 	}
 
 	private void writeMeasurementToFile(ArrayList<InstrumentationData> currentRecord, BufferedWriter out,
-			int indexOfMeasurementInRecord) throws IOException {
-		long measurement = currentRecord.get(indexOfMeasurementInRecord).measurement;
-		if (indexOfMeasurementInRecord == TIME_ID) {
-			Calendar TimeStamp = Calendar.getInstance();
-			SimpleDateFormat TimeStampFormat = new SimpleDateFormat("yyyy MM dd::hh:mm:ss:SSS");
+			int indexInRecord) throws IOException {
+		long measurement = currentRecord.get(indexInRecord).measurement;
+		int id = currentRecord.get(indexInRecord).id;
+		if (id == TIME_ID) {
 			out.newLine();
 			TimeStamp.setTimeInMillis(measurement);
 			out.write(TimeStampFormat.format(measurement) + "\t");
-		}
-		else if(Arrays.asList(IDS_TO_WRITE).contains(indexOfMeasurementInRecord))
+		} else{
 			out.write(Double.toString((Double.longBitsToDouble(measurement))) + "\t");
+			}
 	}
 
 	private boolean isCurrentWildPoint(ArrayList<InstrumentationData> record, int wildPointId) {
-//		System.out.println("\n\nWildID;******;" +wildPointId);
+		System.out.println("\n\n\nwildPointId:"+wildPointId);
 		if (wildPointId == -1)
 			return false;
+		System.out.println("\n\nmeasurement:"+record.get(wildPointId).measurement);
+		System.out.println(record.get(wildPointId).measurement == 1l);
 		return record.get(wildPointId).measurement == 1l;
 
 	}
