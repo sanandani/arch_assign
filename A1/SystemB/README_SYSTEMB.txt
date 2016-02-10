a21a974bdbc69684a2c382aabbcd03945bad53c5
Peng-Peng <Penny@Wenxins-MBP.wv.cc.cmu.edu>
2016-02-09 20:49:40 -0500

    update comments

--- A1/SystemB/README_SYSTEMB.txt
+++ A1/SystemB/README_SYSTEMB.txt
@@ -1,4 +1,4 @@
-To run system B, please 
+To run system C, please 
 1) Copy the test data file to the same folder
 2) Type below command in the terminal, providing the name of your file:
-"java Plumber FlightData.dat"
+"java Plumber SubSetA.dat SubSetB.dat”
--- A1/SystemC/BinaryMergerFilter.java
+++ A1/SystemC/BinaryMergerFilter.java
@@ -1,25 +1,4 @@
 
-/**
- * ****************************************************************************************************************
- * File:MiddleFilter.java
- * Course: 17655
- * Project: Assignment 1
- * Copyright: Copyright (c) 2003 Carnegie Mellon University
- * Versions:
- *	1.0 November 2008 - Sample Pipe and Filter code (ajl).
- *
- * Description:
- *
- * This class serves as an example for how to use the FilterRemplate to create a standard filter. This particular
- * example is a simple "pass-through" filter that reads data from the filter's input port and writes data out the
- * filter's output port.
- *
- * Parameters: 		None
- *
- * Internal Methods: None
- *
- *****************************************************************************************************************
- */
 public class BinaryMergerFilter extends MultiPortFilterFramework {
 
     public static final int NO_OF_INPUT = 2;
@@ -68,4 +47,4 @@ public class BinaryMergerFilter extends MultiPortFilterFramework {
 
     } // run
 
-} // MiddleFilter
+} 
