2ef37b03051e0b720c584d1271d42d6c5fb6822c
sanandani <anandani.shubham@gmail.com>
2016-02-26 19:09:58 -0500

    DBAccessManager Structure

---
+++ A2/EEP/OrderApp/src/DBAccessManager.java
@@ -0,0 +1,19 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shubham
+ */
+public class DBAccessManager {
+   
+}
