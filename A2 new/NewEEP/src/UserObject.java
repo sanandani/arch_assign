2b880ff7a0d400f4400fa285e09ab83e87ff4fba
sanandani <anandani.shubham@gmail.com>
2016-03-01 16:05:11 -0500

    A2 new

---
+++ A2 new/NewEEP/src/UserObject.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Penny
+ */
+public class UserObject {
+
+    private Enum userType;
+    private String token;
+
+    public UserObject(Enum userType, String token) {
+        this.userType = userType;
+        this.token = token;
+    }
+
+    /**
+     * @return the userType
+     */
+    public Enum getUserType() {
+        return userType;
+    }
+
+    /**
+     * @param userType the userType to set
+     */
+    public void setUserType(Enum userType) {
+        this.userType = userType;
+    }
+
+    /**
+     * @return the token
+     */
+    public String getToken() {
+        return token;
+    }
+
+    /**
+     * @param token the token to set
+     */
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+}
