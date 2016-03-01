8142485e6e6a00cb90bed649b43c0abdfc82790b
mewanbanjop-mawroh <mewanbanjop.mawroh@gmail.com>
2016-02-29 20:24:40 -0500

    Uncommented

--- A2/EEP/OrderApp/src/AuthManager.java
+++ A2/EEP/OrderApp/src/AuthManager.java
@@ -13,10 +13,9 @@ public class AuthManager implements AuthManagerInterface{
 
     public  UserObject login(String username, String pwd) {
         UserType ut = dbm.login(username, pwd);
-        System.out.println("DEBUG AuthManager:login userType="+ut);
         if (ut != null) {
             Logger.log("user " + username + " is logging in to the system at " + getTime());
-            return new UserObject(ut, generateToken(username));
+            return new UserObject(dbm.login(username, pwd), generateToken(username));
         } else {
             return new UserObject(null, null);
         }
@@ -29,7 +28,7 @@ public class AuthManager implements AuthManagerInterface{
 
     //simulation assume username has at least 5 characters
     private static String generateToken(String username) {
-        //this hash is a simulation
+        //this hash is a simulation 
         return "u_" + username.substring(0, 3) + "_signed" + username.substring(3);
     }
 
@@ -57,84 +56,84 @@ public class AuthManager implements AuthManagerInterface{
     }
 
     public int insertInventory(String table, String productId, String description, int quantity, float perUnitCost, String token) {
-        //if (isValidToken(token)) {
+        if (isValidToken(token)) {
             return dbm.insertInventory(table,productId,description,quantity,perUnitCost);
-        // } else {
-            // throw new UnsupportedOperationException("Token Invalid");
-        // }
+        } else {
+            throw new UnsupportedOperationException("Token Invalid");
+        }
     }
 
     public int reduceQuantityByOne(String table, String productId, String token) {
-        //if (isValidToken(token)) {
+        if (isValidToken(token)) {
             return dbm.reduceQuantityByOne(table,productId);
-        // } else {
-            // throw new UnsupportedOperationException("Token Invalid");
-        // }
+        } else {
+            throw new UnsupportedOperationException("Token Invalid");
+        }
     }
 
     public int delete(String table, String productId, String token) {
-        //if (isValidToken(token)) {
+        if (isValidToken(token)) {
             return dbm.delete(table,productId);
-        // } else {
-            // throw new UnsupportedOperationException("Token Invalid");
-        // }
+        } else {
+            throw new UnsupportedOperationException("Token Invalid");
+        }
     }
 
     public ResultSet select(String table, String token) {
-        //if (isValidToken(token)) {
+        if (isValidToken(token)) {
             return dbm.select(table);
-        // } else {
-            // throw new UnsupportedOperationException("Token Invalid");
-        // }
+        } else {
+            throw new UnsupportedOperationException("Token Invalid");
+        }
     }
 
     public ResultSet select(String table, String productId, String token) {
-        //if (isValidToken(token)) {
+        if (isValidToken(token)) {
             return dbm.select(table,productId);
-        // } else {
-            // throw new UnsupportedOperationException("Token Invalid");
-        // }
+        } else {
+            throw new UnsupportedOperationException("Token Invalid");
+        }
     }
 
     public int createOrderTable(String orderTableName, String token) {
-        //if (isValidToken(token)) {
+        if (isValidToken(token)) {
             return dbm.createOrderTable(orderTableName);
-        // } else {
-            // throw new UnsupportedOperationException("Token Invalid");
-        // }
+        } else {
+            throw new UnsupportedOperationException("Token Invalid");
+        }
     }
 
-    public int insertOrder(String dateTimeStamp, String firstName, String lastName,
-            String customerAddress, String phoneNumber, float fCost, Boolean shipped,
+    public int insertOrder(String dateTimeStamp, String firstName, String lastName, 
+            String customerAddress, String phoneNumber, float fCost, Boolean shipped, 
             String orderTableName, String token) {
-        //if (isValidToken(token)) {
+        if (isValidToken(token)) {
             return dbm.insertOrder(dateTimeStamp,firstName,lastName,customerAddress,phoneNumber,fCost,shipped,orderTableName);
-        // } else {
-            // throw new UnsupportedOperationException("Token Invalid");
-        // }
+        } else {
+            throw new UnsupportedOperationException("Token Invalid");
+        }
     }
 
     public int dropOrderTable(String orderTableName, String token) {
-        //if (isValidToken(token)) {
+        if (isValidToken(token)) {
             return dbm.dropOrderTable(orderTableName);
-        // } else {
-            // throw new UnsupportedOperationException("Token Invalid");
-        // }
+        } else {
+            throw new UnsupportedOperationException("Token Invalid");
+        }
     }
 
     public int insertOrder(String orderTableName, String productId, String description, float perUnitCost, String token) {
-        //if (isValidToken(token)) {
+        if (isValidToken(token)) {
             return dbm.insertOrder(orderTableName,productId,description,perUnitCost);
-        // } else {
-            // throw new UnsupportedOperationException("Token Invalid");
-        // }
+        } else {
+            throw new UnsupportedOperationException("Token Invalid");
+        }
     }
 
     public int setOrderShipped(String orderId, Boolean shipped, String token) {
-        //if (isValidToken(token)) {
+        if (isValidToken(token)) {
             return dbm.setOrderShipped(orderId,shipped);
-        // } else {
-            // throw new UnsupportedOperationException("Token Invalid");
-        // }
+        } else {
+            throw new UnsupportedOperationException("Token Invalid");
+        }
     }
 }
