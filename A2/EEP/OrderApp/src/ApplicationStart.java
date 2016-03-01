30a8e3961f87851fbccde9e7f10b9b42bbf3e606
mewanbanjop-mawroh <mewanbanjop.mawroh@gmail.com>
2016-02-29 19:27:32 -0500

    Login

--- A2/EEP/OrderApp/src/ApplicationStart.java
+++ A2/EEP/OrderApp/src/ApplicationStart.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JFrame;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,15 +11,25 @@
  *
  * @author mewanbanjop
  */
-public class Login extends javax.swing.JFrame {
+public class ApplicationStart extends javax.swing.JFrame {
 
     AuthManagerInterface authmanager = new AuthManager();
+    JFrame inventory = new InventoryMainFrame();
+        JFrame inventorymanager = new InventoryManagerFrame();
+        JFrame shipping = new ShippingNewJFrame();
+        JFrame order = new NewJFrame();
     /**
      * Creates new form Login
      */
-    public Login() {
+    public ApplicationStart() {
         initComponents();
         jLabel4.setVisible(false);
+        
+        inventory.setVisible(false);
+        inventorymanager.setVisible(false);
+        shipping.setVisible(false);
+        order.setVisible(false);
+
     }
 
     /**
@@ -166,6 +179,16 @@ public class Login extends javax.swing.JFrame {
         }
         else {
           jLabel4.setVisible(false);
+          Enum usertype = user.getUserType();
+          if(usertype == UserType.IT){
+          
+          }
+          else if(usertype == UserType.IT){
+          inventory.setVisible(true);
+          }
+          else{
+          
+          }
         }
         
     }//GEN-LAST:event_jButton1MouseClicked
@@ -187,20 +210,21 @@ public class Login extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ApplicationStart.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ApplicationStart.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ApplicationStart.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ApplicationStart.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Login().setVisible(true);
+                new ApplicationStart().setVisible(true);
             }
         });
     }
