a4b606db1075c5044ac45c0aedad6b2867609b28
mewanbanjop-mawroh <mewanbanjop.mawroh@gmail.com>
2016-03-27 00:43:33 -0400

    Adding shell scripts

---
+++ A3 Archive-2016/System A/4_StartSecuritySystem.sh
@@ -0,0 +1,1 @@
+echo "Starting Alarm Controller" | java AlarmController | echo "Starting Sensors" | java DoorBreakSensor | java WindowBreakSensor | java MotionSensor
--- A3 Archive-2016/System A/StartSecuritySystem.sh
+++
@@ -1,1 +0,0 @@
-echo "Starting Alarm Controller" | java AlarmController | echo "Starting Sensors" | java DoorBreakSensor | echo "Starting Security Console" | java SecurityConsole
