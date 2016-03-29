f1b10200f4aad4985f8bb559361ece292b00cc0f
sanandani <anandani.shubham@gmail.com>
2016-03-29 14:10:35 -0400

    Updated Sys A's batch for Sys B

--- A3 Archive-2016/System B/4_StartSecuritySystem.sh
+++ A3 Archive-2016/System B/4_StartSecuritySystem.sh
@@ -1,1 +1,1 @@
-echo "Starting Alarm Controller" | java AlarmController | echo "Starting Sensors" | java DoorBreakSensor | java WindowBreakSensor | java MotionSensor
+echo "Starting Alarm Controller" | java AlarmController | echo "Starting Sensors" | java DoorBreakSensor | java WindowBreakSensor | java MotionSensor | java SecurityMonitor
