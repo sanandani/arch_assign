3a667a22dfcecfa9e5fdee14b6a48b9fe56023f1
mewanbanjop-mawroh <mewanbanjop.mawroh@gmail.com>
2016-03-28 21:55:40 -0400

    Adding heartbeat

---
+++ A3 Archive-2016/System C/SecurityConsole.java
@@ -0,0 +1,304 @@
+import MessagePackage.Message;
+import MessagePackage.MessageManagerInterface;
+import TermioPackage.Termio;
+
+public class SecurityConsole {
+
+	/***********************
+	 * Private class fields
+	 ***********************/
+	private static MessageManagerInterface messageManager;	// Interface object to the message manager
+	private static boolean Done = false;				// Loop termination flag
+	private static Termio UserInput = new Termio();	// Termio IO Object
+
+	/************
+	 * Constants
+	 ************/
+	private static final int ARM_ID = 100;
+	private static final int DISARM_ID = 101;
+	private static final int HALT_SECURITY_ID = 199;
+	private static final int MOTION_SIMULATE_ID = 160;
+	private static final int DOOR_SIMULATE_ID = 161;
+	private static final int WINDOW_SIMULATE_ID = 162;
+	private static final String SIMULATE_ON = "On";
+	private static final String SIMULATE_OFF = "Off";
+
+	public static void main(String args[])
+	{
+		instantiateMessageManager(args);
+
+		if (messageManager != null)
+		{
+			processControllerMessages(args);
+
+		} else {
+
+			System.out.println("Unable to register with the message manager.\n\n" );
+
+		}
+
+	}
+
+	private static void processControllerMessages(String args[]) {
+		/*****************************************
+		*  Here we start the main console that
+		*  will continuously wait for user input
+		******************************************/
+		String Option = null;
+
+		while (!Done)
+		{
+			//Print Menu
+			printConsoleMenu(args);
+			//Read option
+			Option = UserInput.KeyboardReadString();
+
+			//////////// option 1: Arm security system ////////////
+			if (Option == null) continue;
+			if ( Option.equals( "1" ) )
+			{
+				armSecuritySystem();
+			}
+			////////////option 2: Disarm security system ////////////
+			else if ( Option.equals( "2" ) )
+			{
+				disarmSecuritySystem();
+			}
+			////////////option 3: Simulate Door Break ////////////
+
+			else if ( Option.equals( "3" ) )
+			{
+				simulateDoorBreak();
+			}
+			////////////option 4: Simulate Window Break ////////////
+			else if ( Option.equals( "4" ) )
+			{
+				simulateWindowBreak();
+			}
+			////////////option 5: Simulate Motion detected ////////////
+			else if ( Option.equals( "5" ) )
+			{
+				simulateMotion();
+			}
+			////////////option 6: Exit security system ////////////
+			else if ( Option.equals( "6" ) )
+			{
+				haltSecuritySystem();
+			}
+			else{
+				System.out.println( "Wrong Option" );
+			}
+		}
+	}
+
+	private static void printConsoleMenu(String[] args) {
+		System.out.println( "\n\n\n\n" );
+		System.out.println( "Security Console: \n" );
+		if (args.length != 0)
+			System.out.println( "Using message manager at: " + args[0] + "\n" );
+		else
+			System.out.println( "Using local message manager \n" );
+
+		System.out.println( "Select an Option: \n" );
+		System.out.println( "1: Arm security system" );
+		System.out.println( "2: Disarm security system" );
+		System.out.println( "3: Simulate Door Break" );
+		System.out.println( "4: Simulate Window Break" );
+		System.out.println( "5: Simulate Motion detected" );
+		System.out.println( "6: Exit security system" );
+		System.out.print( "\n>>>> " );
+	}
+
+	private static void haltSecuritySystem() {
+		Done = true;
+
+		try
+		{
+			sendMessageToMessageManager("Halt", HALT_SECURITY_ID );
+			messageManager.UnRegister();
+		}
+
+		catch (Exception e)
+		{
+			System.out.println("Error unregistering: " + e);
+
+		}
+	}
+
+	private static void simulateDoorBreak() {
+		try
+		{
+			sendMessageToMessageManager("On", DOOR_SIMULATE_ID );
+			while(true){
+			System.out.println( "Stop simulation:(y/n) \n" );
+			if(UserInput.KeyboardReadString().equalsIgnoreCase("y")){
+				sendMessageToMessageManager("Off", DOOR_SIMULATE_ID );
+				break;
+			}
+			}
+		}
+
+		catch (Exception e)
+		{
+			System.out.println("Error sending message: " + e);
+
+		}
+	}
+
+	private static void simulateWindowBreak() {
+		try
+		{
+
+			sendMessageToMessageManager(SIMULATE_ON, WINDOW_SIMULATE_ID );
+			while(true){
+			System.out.println( "Stop simulation:(y/n) \n" );
+			if(UserInput.KeyboardReadString().equalsIgnoreCase("y")){
+
+				sendMessageToMessageManager(SIMULATE_OFF, WINDOW_SIMULATE_ID );
+				break;
+			}
+			}
+		}
+
+		catch (Exception e)
+		{
+			System.out.println("Error sending message: " + e);
+
+		}
+	}
+
+	private static void simulateMotion() {
+		try
+		{
+			sendMessageToMessageManager("On", MOTION_SIMULATE_ID );
+			while(true){
+			System.out.println( "Stop simulation:(y/n) \n" );
+			if(UserInput.KeyboardReadString().equalsIgnoreCase("y")){
+				sendMessageToMessageManager("Off", MOTION_SIMULATE_ID );
+				break;
+			}
+			}
+		}
+
+		catch (Exception e)
+		{
+			System.out.println("Error sending message: " + e);
+
+		}
+	}
+
+	private static void armSecuritySystem() {
+		try
+		{
+
+			sendMessageToMessageManager("Arm", ARM_ID );
+		}
+
+		catch (Exception e)
+		{
+			System.out.println("Error sending message: " + e);
+
+		}
+	}
+
+	private static void disarmSecuritySystem() {
+		try
+		{
+			sendMessageToMessageManager("Disarm", DISARM_ID );
+		}
+
+		catch (Exception e)
+		{
+			System.out.println("Error sending message: " + e);
+
+		}
+	}
+
+	private static void instantiateMessageManager(String[] args) {
+
+		if ( args.length == 0 ) // Checking if No IP is provided
+ 		{
+			// message manager is on the local system
+
+			System.out.println("\n\nAttempting to register on the local machine..." );
+
+			try
+			{
+				// Here we create an message manager interface object. This assumes
+				// that the message manager is on the local machine
+
+				messageManager = new MessageManagerInterface();
+			}
+
+			catch (Exception e)
+			{
+				System.out.println("Error instantiating message manager interface: " + e);
+
+			}
+
+		}
+
+		else {
+
+			// message manager is not on the local system
+
+			String MsgMgrIP = args[0];
+
+			System.out.println("\n\nAttempting to register on the machine:: " + MsgMgrIP );
+
+			try
+			{
+				// Here we create an message manager interface object. This assumes
+				// that the message manager is NOT on the local machine
+
+				messageManager = new MessageManagerInterface( MsgMgrIP );
+			}
+
+			catch (Exception e)
+			{
+				System.out.println("Error instantiating message manager interface: " + e);
+
+			}
+
+		}
+	}
+
+	static private void sendMessageToMessageManager(String msg, int id)
+	{
+
+		Message message = new Message( id, msg); // Here we create the message.
+
+		try
+		{
+			messageManager.SendMessage( message ); // Here we send the message to the message manager.
+
+		}
+
+		catch (Exception e)
+		{
+			System.out.println("Error Confirming Message:: " + e);
+
+		}
+
+	}
+
+	static private void sendHeartBeat(MessageManagerInterface ei, String ID,String DeviceName, String DeviceDescription){
+           // Here we create the message.
+
+        Message msg = new Message( (int) 0, ID + ":" + DeviceName + ":" + DeviceDescription);
+
+        // Here we send the message to the message manager.
+
+        try
+        {
+            ei.SendMessage( msg );
+
+        } // try
+
+        catch (Exception e)
+        {
+            System.out.println("Error Registering the device :: " + e);
+
+        } // catch
+       }
+}
---
+++ A3 Archive-2016/System C/WindowBreakSensor.java
@@ -0,0 +1,270 @@
+import InstrumentationPackage.MessageWindow;
+import MessagePackage.Message;
+import MessagePackage.MessageManagerInterface;
+import MessagePackage.MessageQueue;
+
+public class WindowBreakSensor {
+	/***********************
+	 * Private class fields
+	 ***********************/
+	private static MessageManagerInterface messageManager;	// Interface object to the message manager
+	private static MessageWindow messageWindow;
+	private static MessageQueue queue;				// Message Queue
+	private static Message Msg;					// Message object
+	private static boolean WindowBreakSensorState = false;	// Door Break Sensor State : false == off, true == on
+	private static int	Delay = 2500;					// The loop delay (2.5 seconds)
+	private static boolean Done = false;				// Loop termination flag
+
+	/************
+	 * Constants
+	 ************/
+
+	private static final int WINDOW_BREAK_SENSOR_ID = -112;
+	private static final String WINDOW_BREAK_SENSOR_ON = "W1";
+	private static final String WINDOW_BREAK_SENSOR_OFF = "W0";
+	private static final int HALT_SECURITY_ID = 199;
+	private static final int WINDOW_BREAK_MSG_ID = 122;
+	private static final String WINDOW_BREAK_DETECTED = "WINDOW BREAK DETECTED";
+	private static final String OK = "OK";
+	private static final int WINDOW_SIMULATE_ID = 162;
+	private static final String SIMULATE_ON = "On";
+
+
+
+	public static void main(String args[])
+	{
+		instantiateMessageManager(args);
+
+		// Here we check to see if registration worked. If messageManager is null then the
+		// message manager interface was not properly created.
+
+		if (messageManager != null)
+		{
+			initializeDisplays();
+			performSensorprocess();
+
+		} else {
+
+			System.out.println("Unable to register with the message manager.\n\n" );
+
+		}
+
+	}
+
+	private static void performSensorprocess() {
+		messageWindow.WriteMessage("Window Break Sensor off" );
+		/**************************************************
+		*  Here we start the main simulation loop that
+		*  will continuously look for control messages
+		***************************************************/
+
+		while ( !Done )
+		{
+			//postArmStatus
+
+			try
+			{
+				queue = messageManager.GetMessageQueue(); //get messages from message manager
+				sendHeartBeat(em,"173","Window Break Sensor","This is a Window Break Sensor");
+			}
+
+			catch( Exception e )
+			{
+				messageWindow.WriteMessage("Error getting message queue::" + e );
+			}
+
+			int qlen = queue.GetSize();
+
+			for ( int i = 0; i < qlen; i++ )
+			{
+				Msg = queue.GetMessage();
+
+				if ( Msg.GetMessageId() == WINDOW_BREAK_SENSOR_ID)
+				{
+					handleWindowBreakControllerMessage(Msg);
+				}
+
+				if ( Msg.GetMessageId() == HALT_SECURITY_ID )
+				{
+					handleExitMessage();
+				}
+				if ( WindowBreakSensorState && Msg.GetMessageId() == WINDOW_SIMULATE_ID )
+				{
+					if(SIMULATE_ON.equals(Msg.GetMessage())){
+						sendMessageToMessageManager(WINDOW_BREAK_DETECTED,WINDOW_BREAK_MSG_ID);
+					}
+					else{
+						sendMessageToMessageManager(OK,WINDOW_BREAK_MSG_ID);
+					}
+				}
+
+			}
+
+			try
+			{
+				Thread.sleep( Delay );
+			}
+
+			catch( Exception e )
+			{
+				System.out.println( "Sleep error:: " + e );
+
+			}
+		}
+	}
+
+
+	private static void handleExitMessage() {
+		Done = true;
+
+		try
+		{
+			messageManager.UnRegister();
+
+		}
+
+		catch (Exception e)
+		{
+			messageWindow.WriteMessage("Error unregistering: " + e);
+
+		}
+
+		messageWindow.WriteMessage( "\n\nSimulation Stopped. \n");
+
+	}
+
+	private static void handleWindowBreakControllerMessage(Message Msg) {
+
+
+		if (Msg.GetMessage().equalsIgnoreCase(WINDOW_BREAK_SENSOR_ON)) // window break Sensor on
+		{
+			messageWindow.WriteMessage("Window Break Sensor on" );
+			WindowBreakSensorState = true;
+
+		}
+
+		if (Msg.GetMessage().equalsIgnoreCase(WINDOW_BREAK_SENSOR_OFF)) // window break Sensor off
+		{
+			messageWindow.WriteMessage("Window Break Sensor off" );
+			WindowBreakSensorState = false;
+		}
+	}
+
+
+
+
+	private static void initializeDisplays() {
+
+		System.out.println("Registered with the message manager." );
+
+		// Now we create the window break Sensor status and message panel
+
+		float WinPosX = 0.10f; 	//This is the X position of the message window in term of a percentage of the screen height
+		float WinPosY = 0.90f;	//This is the Y position of the message window in terms of a percentage of the screen height
+
+		messageWindow = new MessageWindow("Window Break Sensor", WinPosX, WinPosY);
+
+		// Now we put the indicators directly under the panel
+
+		messageWindow.WriteMessage("Registered with the message manager." );
+
+		try
+		{
+			messageWindow.WriteMessage("   Participant id: " + messageManager.GetMyId() );
+			messageWindow.WriteMessage("   Registration Time: " + messageManager.GetRegistrationTime() );
+		}
+
+		catch (Exception e)
+		{
+			System.out.println("Error:: " + e);
+		}
+	}
+
+	private static void instantiateMessageManager(String[] args) {
+
+		if ( args.length == 0 ) // Checking if No IP is provided
+ 		{
+			// message manager is on the local system
+
+			System.out.println("\n\nAttempting to register on the local machine..." );
+
+			try
+			{
+				// Here we create an message manager interface object. This assumes
+				// that the message manager is on the local machine
+
+				messageManager = new MessageManagerInterface();
+			}
+
+			catch (Exception e)
+			{
+				System.out.println("Error instantiating message manager interface: " + e);
+
+			}
+
+		}
+
+		else {
+
+			// message manager is not on the local system
+
+			String MsgMgrIP = args[0];
+
+			System.out.println("\n\nAttempting to register on the machine:: " + MsgMgrIP );
+
+			try
+			{
+				// Here we create an message manager interface object. This assumes
+				// that the message manager is NOT on the local machine
+
+				messageManager = new MessageManagerInterface( MsgMgrIP );
+			}
+
+			catch (Exception e)
+			{
+				System.out.println("Error instantiating message manager interface: " + e);
+
+			}
+
+		}
+	}
+
+	static private void sendMessageToMessageManager(String msg, int id)
+	{
+
+		Message message = new Message( id, msg); // Here we create the message.
+
+		try
+		{
+			messageWindow.WriteMessage(msg);
+			messageManager.SendMessage( message ); // Here we send the message to the message manager.
+
+		}
+
+		catch (Exception e)
+		{
+			System.out.println("Error Sending Message:: " + e);
+
+		}
+
+	}
+	static private void sendHeartBeat(MessageManagerInterface ei, String ID,String DeviceName, String DeviceDescription){
+           // Here we create the message.
+
+        Message msg = new Message( (int) 0, ID + ":" + DeviceName + ":" + DeviceDescription);
+
+        // Here we send the message to the message manager.
+
+        try
+        {
+            ei.SendMessage( msg );
+
+        } // try
+
+        catch (Exception e)
+        {
+            System.out.println("Error Registering the device :: " + e);
+
+        } // catch
+       }
+}
